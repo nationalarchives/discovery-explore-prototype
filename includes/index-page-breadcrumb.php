c194d6623e829c121bb2f53d9a43813fee069733
Gwyn Jones <m@gwnjns.com>
2017-06-01 15:54:07 +0100

    Add existing Discovery page controls

--- includes/index-page-breadcrumb.php
+++ includes/index-page-breadcrumb.php
@@ -10,8 +10,23 @@
         </nav>
     </div>
     <div class="col starts-at-full ends-at-half clr">
-        <?php
-            // To include Discovery-like page controls simply introduce them as nav#page-controls > ul > li > a
-        ?>
+        <nav id="page-controls">
+
+            <ul>
+                <li><a href="/" class="startNewSearch">Start new search</a></li>
+                <li>
+                    <a class="printThisPage" href="javascript:window.print()" title="print">Print</a>
+                </li>
+                <li>
+                    <a href="http://www.nationalarchives.gov.uk/help-with-your-research/discovery-help" target="_blank" id="helpLink">
+                        Discovery help
+                    </a>
+                </li>
+
+
+
+                <a href="/sign-in" id="bookmarkLink">Bookmark</a>
+            </ul>
+        </nav>
     </div>
 </section>
