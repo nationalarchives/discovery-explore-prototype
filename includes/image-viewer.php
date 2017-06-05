2c7597239e14e141bc2774df3ab17cafbf90c15b
Gwyn Jones <m@gwnjns.com>
2017-06-05 14:57:25 +0100

    Add image viewer

---
+++ includes/image-viewer.php
@@ -0,0 +1,4 @@
+<div class="viewer-message">
+    <h2 class="hidden-but-accessible">Image viewer</h2>
+    <p>This record is available to view with our image viewer. <a id="imageViewerLink" href="#imageViewerLink" role="button">Show images</a></p>
+</div>
--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -4,6 +4,7 @@
             <h1 class="inline"><?= $record['title'] ?></h1>
             <?php include('hierarchy.php') ?>
             <?php include('asset-details.php') ?>
+            <?php include('image-viewer.php') ?>
         </div>
     </div>
     <div class="col starts-at-full ends-at-one-third clr holding-box">
