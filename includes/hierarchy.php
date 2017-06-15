b45bc33fde5731fb045a20cdbe0383ebe8725fb7
Gwyn Jones <m@gwnjns.com>
2017-06-15 09:23:53 +0100

    Possible option to allow for browse from current item
    This option:
    
    - Moves the heading below hierarchy to illustrate structure
    - Introduces a browse link next to the heading

--- includes/hierarchy.php
+++ includes/hierarchy.php
@@ -3,4 +3,5 @@
         <a href="#">The National Archives</a>
         <?php render_breadcrumb($record['breadcrumbs']); ?>
     </ul>
+    <h1 class="inline"><?= $record['title'] ?></h1><a href="#">Browse from this record</a>
 </div>
