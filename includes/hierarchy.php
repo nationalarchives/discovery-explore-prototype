c3ba80703b321a81bc359f01b60b3371a685083c
Gwyn Jones <m@gwnjns.com>
2017-06-20 16:56:26 +0100

    Indent heading to reflect relationship to hierarchy
    Brings the h1 into line with the parent context to better illustrate
    that the current item is a descendent of the items show in the
    context.

--- includes/hierarchy.php
+++ includes/hierarchy.php
@@ -2,6 +2,6 @@
     <ul>
         <a href="#">The National Archives</a>
         <?php render_breadcrumb($record['breadcrumbs']); ?>
+        <li><h1 class="inline"><?= $record['title'] ?></h1><a href="#">Browse from this record</a></li>
     </ul>
-    <h1 class="inline"><?= $record['title'] ?></h1><a href="#">Browse from this record</a>
 </div>
