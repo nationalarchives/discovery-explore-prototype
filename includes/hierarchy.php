b0d28182019018754979346808b4e69b1b67bd47
Gwyn Jones <m@gwnjns.com>
2017-06-28 12:40:30 +0100

    Change text for root of hierarchy
    Feedback from the show-and-tell today suggested that it might help
    orientate users if 'The National Archives' was changed to 'The
    National Archives collections' because:
    
    - It makes it clear the link takes you to a view of collections,
      rather than the TNA homepage
    - This gives context to the hierarchy as a whole being a view
      of collections

--- includes/hierarchy.php
+++ includes/hierarchy.php
@@ -1,6 +1,6 @@
 <div id="hierarchy">
     <ul>
-        <a href="#">The National Archives</a>
+        <a href="#">The National Archives collections</a>
         <?php render_breadcrumb($record['breadcrumbs']); ?>
         <li><h1 class="inline"><?= $record['title'] ?></h1><a href="#">Browse from this record</a></li>
     </ul>
