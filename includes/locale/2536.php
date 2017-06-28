8717cfa0287b207c16191e583e4588cd6814a44d
Gwyn Jones <m@gwnjns.com>
2017-06-28 12:35:21 +0100

    Bug fix: removes 'all departments' from hierarchy

--- includes/locale/2536.php
+++ includes/locale/2536.php
@@ -11,7 +11,6 @@ $record = [
     'title' => 'Report on speech secrecy system DELILAH, a technical description compiled by A M Turing...',
     'reference' => 'HW 25/36',
     'breadcrumbs' => [
-        'All departments',
         'HW - Records created or inherited by Government Communications Headquarters (GCHQ)',
         'Division within HW - General records of the Government Code and Cypher School',
         'HW 25 - Government Code and Cypher School: Cryptographic Studies',
